a5c7ae313adba11ab991d94f0e05f595ba7666b1
Christian Jimenez <cajt@outlook.com>
2025-01-07 12:01:20 -0800

    Slider done, responsive done

--- final-project/src/js/help.js
+++ final-project/src/js/help.js
@@ -1,11 +1,16 @@
 // if the window is larger than 1200
-if(window.innerWidth >= 1200){
+if((window.innerWidth-34) > 1200){
+    console.log(window)
     book()
+    if(!document.querySelector(".img-form")){
+        image()  
+    }
 }
 
 // if the user resize the screeen
 window.addEventListener("resize", ()=>{
-    if(window.innerWidth >= 1200){
+    image()
+    if(window.innerWidth > 1200){
         book()
     }
     else{
@@ -24,3 +29,21 @@ function quote(){
     let book = document.querySelector(".quote")
         book.textContent = "Request a quote"
 }
+
+// fucntion to add the image and remove it 
+function image(){
+    let cont = document.querySelector(".image-form")
+    if((window.innerWidth-34) < 1200){
+        if(document.querySelector(".img-form")){
+            document.querySelector(".img-form").remove() 
+        }
+    }else{
+        if(!document.querySelector(".img-form")){
+            let img = document.createElement("img")
+            img.classList.add("img-form")
+            img.src = "../../src/images/form-image/Image.png"
+            cont.append(img)   
+        }
+    }
+}
+
